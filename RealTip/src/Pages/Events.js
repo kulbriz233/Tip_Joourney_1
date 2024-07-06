807498b25186007a487187f3d411096fe53ef36f
KB <92556407+kulbriz233@users.noreply.github.com>
2024-07-06 18:59:30 +0000

    Work in progress

---
+++ RealTip/src/Pages/Events.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="">
+      <label>Full Name</label>
+      <input type='name' placeholder='Name'></input>
+      
+    </div>
+  );
+};
+
+export default Events;
